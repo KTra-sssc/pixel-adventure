9454c83b77569bc98e79da63bd59424bf18a0331
KTra <kt7456158@gmail.com>
2023-11-21 00:20:28 +0700

    flip player

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,30 +7,43 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D rbody; 
     private Animator animator;
+    private float dirX = 0f;
+    private SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
+        dirX = Input.GetAxisRaw("Horizontal");
         rbody.velocity = new Vector2(dirX * 7f, rbody.velocity.y);
         if (Input.GetButtonDown("Jump"))
         {
             rbody.velocity = new Vector2(rbody.velocity.x, 15);
         }
+        switchAnimation();
+    }
 
-        if (dirX != 0f)
+    void switchAnimation()
+    {
+        if (dirX < 0f)
         {
-            animator.SetBool("run", true); 
+            animator.SetBool("run", true);
+            spriteRenderer.flipX = true;
+        }
+        else if (dirX > 0f)
+        {
+            animator.SetBool ("run", true);
+            spriteRenderer.flipX = false;
         }
         else
         {
-            animator.SetBool ("run", false);
+            animator.SetBool("run", false);
         }
     }
 }

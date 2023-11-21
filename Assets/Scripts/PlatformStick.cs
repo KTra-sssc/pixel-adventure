33cc5e07ceeb47faee7a0aa137ff74615dfa5ae2
KTra <kt7456158@gmail.com>
2023-11-21 14:16:10 +0700

    box collider 2nd

--- Assets/Scripts/PlatformStick.cs
+++ Assets/Scripts/PlatformStick.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformStick : MonoBehaviour
-{
-    private void OnCollisionEnter2D(Collision2D collision)
+{ 
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.name == "Player")
+        if (collision.gameObject.name == "Player")
         {
             collision.gameObject.transform.SetParent(transform);
         }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.name == "Player")
         {
